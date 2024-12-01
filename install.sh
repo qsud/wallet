134323b959ca4e18a8c361268676fd3286663c0e
ARQAM 🇵🇸 <107029929+qsud@users.noreply.github.com>
2024-12-01 15:21:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/image/main.py",
+    "postStartCommand": "python3 /workspaces/wallet/m.py",
     "customizations": {
         "vscode": {
             "settings": {
